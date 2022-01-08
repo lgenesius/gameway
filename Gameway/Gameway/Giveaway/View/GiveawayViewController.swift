6eca4c601f3798d39f90326418f591865cda637b
Luis Genesius <luis.huang321@gmail.com>
2022-01-08 21:02:05 +0700

    add giveaway view controller

---
+++ Gameway/Gameway/Giveaway/View/GiveawayViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GiveawayViewController.swift
+//  Gameway
+//
+//  Created by Luis Genesius on 08/01/22.
+//
+
+import UIKit
+
+class GiveawayViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
